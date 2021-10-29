6d3d84876fdaa05d70588089738761a29438c14f
ChunghinLam <clam141@my.centennialcollege.ca>
2021-10-29 01:41:17 -0400

    update book add/edit functions

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://dba:dba@cluster0.4xrc5.mongodb.net/book_store?retryWrites=true&w=majority"
 };
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,7 +1,9 @@
 // modules required for routing
 let express = require('express');
+const { copyFileSync } = require('fs');
 let router = express.Router();
 let mongoose = require('mongoose');
+const { title } = require('process');
 
 // define the book model
 let book = require('../models/books');
@@ -20,50 +22,82 @@ router.get('/', (req, res, next) => {
       });
     }
   });
-
 });
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    res.render('books/details', { title: 'Add Book' });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
+  let newBook = book({
+    "Title": req.body.title,
+    "Description": req.body.description,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  book.create(newBook, (err, book) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect('/books');
+    }
+  });
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  book.findById(id, (err, bookToEdit) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.render('books/details', { title: 'Edit Book', books: bookToEdit });
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
+  let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  let updatedBook = book({
+    "_id": id,
+    "Title": req.body.title,
+    "Description": req.body.description,
+    "Author": req.body.author,
+    "Price": req.body.price,
+    "Genre": req.body.genre
+  });
+  
+  book.updateOne({ _id: id }, updatedBook, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect('/books');
+    }
+  })
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
+  let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  book.deleteOne({ _id: id }, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      res.redirect('/books');
+    }
+  })
 });
 
 
